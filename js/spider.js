b038ba84a1106c3f3c3b5c4e36cc6f69166a822b
orrin <orrin.rhodes@outlook.com>
2023-01-01 23:59:02 -0600

    graph

--- js/spider.js
+++ js/spider.js
@@ -10,10 +10,18 @@ const deg = circle/360
 
 const clamp = (num, min, max) => Math.min(Math.max(num, min), max);
 
+const plotText =(p)=>{
+    ctx.fillStyle = 'white';
+    ctx.font = '18px Courier New';
+    ctx.fillText(
+        `(${p.x},${p.y})`,
+        p.x+10,p.y
+    );
+}
 
 // clamp(x2, x1-offset,x1+offset),
 // clamp(y2, y1-offset,y1+offset),
-const arrow=(p0, p1, w, color, first)=>{
+const arrow=(p0, p1, w, color, last)=>{
 
     ctx.strokeStyle = color;
     ctx.fillStyle = color;
@@ -26,59 +34,76 @@ const arrow=(p0, p1, w, color, first)=>{
     ctx.stroke();
 
     // points
-    if(first){
-        ctx.beginPath();
-        ctx.arc(
-            p0.x,
-            p0.y,
-            w*1.5,
-            0*deg,
-            circle,
-            false
-        );
-        ctx.fill();
-    };
-
     ctx.beginPath();
     ctx.arc(
-        p1.x,
-        p1.y,
+        p0.x,
+        p0.y,
         w*1.5,
         0*deg,
         circle,
         false
     );
     ctx.fill();
+
+    if(last){
+        ctx.beginPath();
+        ctx.arc(
+            p1.x,
+            p1.y,
+            w*1.5,
+            0*deg,
+            circle,
+            false
+        );
+        ctx.fill();
+        
+        plotText(p1);
+    };
+
+    plotText(p0);
 };
 
 const hexagon = [
     {x:50, y:50},
     {x:150, y:150},
-    {x:300, y:100},
-    {x:350, y:300},
+    {x:300, y:30},
+    {x:400, y:300},
+    {x:200, y:250},
+    {x:50, y:325},
+    {x:250, y:325},
+    {x:50, y:200},
 ];
+
+const colors = [
+    'orange',
+    'yellow',
+    'red',
+    'green',
+    'blue',
+];
+
+// const randomItem =(list)=> {
+//     return list[Math.floor(Math.random()*list.length)];
+// };
 
 const draw=()=>{
     spiderCanvas.width = width; // px
     spiderCanvas.height = height;
 
     var p0;
-    var p1;
+    var colorIndex = -1;
+    // do in opposite order so bubbles dont overlap
     Object.keys(hexagon).reverse().forEach(point=>{
-        console.log(point, hexagon[point]);
         if(!p0){
-            console.log('not yer')
             p0 = hexagon[point];
         } else{
-            arrow(p0, hexagon[point], 2, 'skyblue');
+            // loop through colors         
+            colorIndex = (colorIndex + 1) % colors.length;
+            // plot point!
+            arrow(p0, hexagon[point], 5,colors[colorIndex], point == 0);
+            p0 = hexagon[point];
         };
     });
-
-    // do in opposite order so bubbles dont overlap
-    // arrow(p2, p3, 5, 'yellow')
-    // arrow(p1, p2, 5, 'red');
-    // arrow(p0, p1, 5, 'yellowgreen', true);
-    
 };
 
 draw();
